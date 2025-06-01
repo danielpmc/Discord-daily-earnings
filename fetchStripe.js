953521dad13d461d51b1e3bb7c818d446348cba0
danielpmc <danielpd93@gmail.com>
2025-06-01 01:29:03 +0100

    Create fetchStripe.js

---
+++ fetchStripe.js
@@ -0,0 +1,28 @@
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+async function getStripeEarningsUSD() {
+  const now = new Date();
+  const todayStart = new Date(now.setHours(0, 0, 0, 0)) / 1000;
+  const todayEnd = new Date(now.setHours(23, 59, 59, 999)) / 1000;
+
+  const charges = await stripe.charges.list({
+    created: {
+      gte: todayStart,
+      lte: todayEnd,
+    },
+    limit: 100,
+  });
+
+  let total = 0;
+  charges.data.forEach(charge => {
+    if (charge.paid && !charge.refunded) {
+      const amountUSD = charge.currency !== 'usd' ? 0 : charge.amount / 100;
+      total += amountUSD;
+    }
+  });
+
+  return total.toFixed(2);
+}
+
+module.exports = { getStripeEarningsUSD };
